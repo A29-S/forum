5f8975845fc5be559b01a7dd66a52dc334d8168d
CodingKingPin <49843971+CodingKingPin@users.noreply.github.com>
2020-04-29 19:03:55 -0400

    Query and Comment Section(not added)
    Making Queries
    https://docs.djangoproject.com/en/3.0/topics/db/queries/
    
    Comment Section
    https://djangocentral.com/creating-comments-system-with-django/

---
+++ website/users/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages 
+from .forms import UserSignUpForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def signUp(request):
+	if request.method == 'POST':
+		form = UserSignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			messages.success(request, f'Account has been Created!!')
+			return redirect('login')
+	else:
+		form = UserSignUpForm()
+	return render(request, 'users/signup.html', {'form': form})
+
+
+@login_required
+def user_profile(request):
+	if request.method == 'POST':
+		user_form = UserUpdateForm(request.POST, instance = request.user)
+		profile_form = ProfileUpdateForm(request.POST, request.FILES, instance = request.user.profile)
+
+		if user_form.is_valid() and profile_form.is_valid():
+			user_form.save()
+			profile_form.save()
+			messages.success(request, f'Account has been Updated')
+			return redirect('user_profile')
+
+	else:
+		user_form = UserUpdateForm(instance = request.user)
+		profile_form = ProfileUpdateForm(instance = request.user.profile)
+
+	context = {
+		'user_form': user_form,
+		'profile_form': profile_form
+	}
+
+	return render(request, 'users/user_profile.html', context)
